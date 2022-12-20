fa9e4a81a88d9e80d19f7c676f998e1c3b9aa380
Toby Hayward <tobyhayward13@gmail.com>
2022-12-21 11:43:55 +1300

    Bug Fixes

--- SK4FGA/R/calculate_lambda_threshold.R
+++ SK4FGA/R/calculate_lambda_threshold.R
@@ -12,7 +12,9 @@
 #'
 calculate_lambda_threshold <- function(k, alpha, .nsims = 1e4) {
   X = matrix(generate_indices(.nsims * k), nrow = .nsims)
+  # Sort each row.
+  X = t(apply(X, 1, sort))
   Y = unlist(apply(X, 1, function(r) find_B0(r)$x))
-  quantile(pi/(2*(pi-2)) * Y/1.6e-9, 1-alpha)
+  quantile(pi/(2*(pi-2)) * Y/(1.6e-9), 1-alpha)
 }
 
--- SK4FGA/R/calculate_lambda_threshold_C.R
+++ SK4FGA/R/calculate_lambda_threshold_C.R
@@ -12,7 +12,9 @@
 #'
 calculate_lambda_threshold_C <- function(k, alpha, .nsims = 1e4) {
   X = matrix(generate_indices(.nsims * k), nrow = .nsims)
+  # Sort each row.
+  X = t(apply(X, 1, sort))
   Y = unlist(apply(X, 1, function(r) find_B0_C(r)$x))
-  quantile(pi/(2*(pi-2)) * Y/1.6e-9, 1-alpha)
+  quantile(pi/(2*(pi-2)) * Y/(1.6e-9), 1-alpha)
 }
 
