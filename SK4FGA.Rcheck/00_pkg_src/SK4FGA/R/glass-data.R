1eb52629634041a0db043152ba33aaf783518d45
Toby Hayward <tobyhayward13@gmail.com>
2023-01-09 11:36:36 +1300

    Wrong files

--- SK4FGA.Rcheck/00_pkg_src/SK4FGA/R/glass-data.R
+++
@@ -1,8 +0,0 @@
-#' Glass Fragment Elemental Composition Data
-#'
-#' A dataset comprised of various glass fragment samples including multiple measurements of 7 element concentrations.
-#'
-#' @format ## `who`
-#'
-#' @source Unknown
-"glass"
