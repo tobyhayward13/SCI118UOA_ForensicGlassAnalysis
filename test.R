7db22837294fdbc203cbba9d38f47296de926227
Toby Hayward <tobyhayward13@gmail.com>
2023-01-23 23:25:44 +1300

    Current test file that is up to date with current build

--- test.R
+++ test.R
@@ -1,28 +1,22 @@
 # A test file to test our package out.
 
 library(SK4FGA)
-
+library(tidyverse)
 
 ri <- generate_indices(20, .sd_multi = 5)
+
 ri
 
 part = partition(ri)
-part.c = partition_C(ri)
+plot(part)
 
+test.data = tibble(glass)
+prepare_data(test.data)
+undebug(prepare_data)
 
+part = partition.multi(test.data)
 
-# ungroup.partition(part$tree)
-
-plot_tree(part)
-plot_tree(part.c)
-class(part.c)
-
-# Don't do. If do, change name of build_tree (plot_tree) to (plot.sk_partition_tree)
-# .S3method('plot', 'sk_partition_tree', plot.sk_partition_tree)
-
-# plot(part.c)
-
-# Testing
+plot(part)# Testing
 
 # Data import
 # data = read.csv('glass.csv')
@@ -38,44 +32,45 @@ data.test = data.prep[1:3]
 
 # debug(partition.multi)
 result = partition.multi(data.test)
+
 # undebug(ungroup.partition)
 # ungroup.partition(result$tree)
 
-plot_tree(result)
+plot(result)
 
 
 # Test 2
 
-data.test = data.prep[[1]]
-data.test = split(data.test, factor(data.test$fragment))
+data.test = prepare_data(data, c('item', 'fragment'))[1:4]
 
 # debug(partition.multi)
 result = partition.multi(data.test)
 
-# undebug(plot_tree)
-plot_tree(result)
+# undebug(plot)
+plot(result)
 
 # Test 3
 
 set.seed(123)
 data.test = data.prep[sample(1:200, 10)]
 result = partition.multi(data.test)
-plot_tree(result)
+plot(result)
 
 
 # Test 4
 # This is dangerous since testing between fragments from the same sample can have issues with high correlation and hence very small covariance.
-# set.seed(123)
-# data.test = data.prep[sample(1:200, 3)]
-data.test = data.prep[c(41, 188, 163)]
-data.test = do.call(rbind, data.test)
-data.test = transform(data.test, item = paste0(item, fragment))
-data.test = prepare_data(data.test)
 
 
-result = partition.multi(data.test)
-plot_tree(result)
+data.prep = data %>% prepare_data(1:2)
+# sam = '677 678 679 680 545 546 547 548 653 654 655 656' %>% str_split('[:space:]') %>% unlist() %>% as.numeric() # Confuses 1
+# sam = '301 302 303 304 249 250 251 252 561 562 563 564' %>% str_split('[:space:]') %>% unlist() %>% as.numeric() # Confuses 2, cant find significance between two. Can create more splits by decreasing alpha.
+# sam = '149 150 151 152  81  82  83  84 161 162 163 164' %>% str_split('[:space:]') %>% unlist() %>% as.numeric() # Confuses 1
+sam = rep(sample(1:200, 3)*4 - 3, each = 4) + 0:3
+data.test = data.prep[sam]
 
+result = partition.multi(data.test, alpha = 0.05)
+result$groups
+plot(result)
 
 
 
@@ -149,7 +144,7 @@ data.test$ref.items
 
 result = partition.multi(data.test$data)
 
-plot_tree(result)
+plot(result)
 
 # Now when combined with the data that was used to generate the parametric bootstrapped data.
 
@@ -163,7 +158,7 @@ data.test$ref.items[1]
 
 result = partition.multi(data.test3)
 table(result$groups)
-plot_tree(result)
+plot(result)
 
 # It is very sensitive. It shouldn't be discriminating between the two samples.
 
@@ -176,13 +171,13 @@ plot_tree(result)
 
 # Test 6
 # Using the same data twice.
-
-data.test = rep(data.prep[1:3], each = 2)
+data.prep = prepare_data(glass, 1)
+data.test = rep(data.prep[sample(length(data.prep), 3)], each = 2)
 names(data.test) = paste0(names(data.test), '.', 1:2)
 data.test
 
 result = partition.multi(data.test)
-plot_tree(result)
+plot(result)
 
 # It only seems to discriminate when the data matches entirely. This is not ideal.
 # This is a result of a stupid assumption that the data is ordered and hence will assume two likely equal items to be different. Observe following example.
@@ -190,6 +185,59 @@ plot_tree(result)
 # Random sample based on before
 data.test = data.test[sample(6)]
 result = partition.multi(data.test)
-plot_tree(result)
+plot(result)
 
 # Notice now that because the two datasets are not next to eachother in the list, they are discriminated apart and hence considered different.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+data
+
+prepare_data <- function(data, label = NA){
+  if (length(label) < 2) if (is.na(label)) {
+    warning('No column assigned to be the label. First column is used.')
+    label = 1L
+  }
+  # Convert to numeric (just easier)
+  if (typeof(label) == 'character') label = which(colnames(data) %in% label)
+  if (length(label) > 1) {
+    items = apply(data[,label], 1, paste, collapse = '.')
+    data = cbind(items, data[,-label])
+  }
+  else items = factor(data[,label])
+
+  split(data[,which(sapply(data, class) == 'numeric')], items)
+}
+
+data = glass
+data = prepare_data(data, 'item')
+
+data.test = data[sample(1:length(data), 3)]
+
+part = partition.multi(data.test)
+plot(part)
+part$groups
+
+ri = generate_indices(.sd_multi = 5)
+ri
+Q
+part = partition(ri)
+plot(part)
+
+# undebug(prepare_data)
+data(glass)
+data.multi = prepare_data(glass, 1)[1:3]
+part = partition.multi(data.multi)
+plot(part)
+
